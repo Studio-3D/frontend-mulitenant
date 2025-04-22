95a8de5fec9d6435ee87bba9c4de35ffe2711f1c
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2025-04-22 12:45:37 +0100

    creating left and right side of tableau de bord

--- src/app/(dashboard)/Tableau-de-Bord/page.js
+++ src/app/(dashboard)/Tableau-de-Bord/page.js
@@ -3,8 +3,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Tableau de Bord
+    <div className=' flex flex-col gap-4 md:flex-row'>
+      {/* left */}
+      <div className='bg-gray-200  w-full lg:w-2/3 '>Content Area</div>
+      {/* right */}
+      <div className='bg-green-200 w-full lg:w-1/3 '>Content Area</div>
+      
     </div>
   )
 }
