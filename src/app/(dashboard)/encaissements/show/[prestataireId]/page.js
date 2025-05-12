2a16869a4f9554ec0df51ac7408eae5fc65d7a9b
unknown <aissiouinabila@gmail.com>
2025-05-12 22:51:32 +0100

    Encaissementt

--- src/app/(dashboard)/encaissements/show/[prestataireId]/page.js
+++
@@ -1,6 +0,0 @@
-import ViewPrestataire from './ViewPrestataire'
-
-export default function Page({ params }) {
-  const { prestataireId } = params
-  return <ViewPrestataire prestataireId={prestataireId} />
-}
