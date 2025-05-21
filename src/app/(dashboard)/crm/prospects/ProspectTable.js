9131f03bf3af0423308c2c79d7e7062f8eebe936
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2025-05-21 16:46:45 +0100

    adding table background

--- src/app/(dashboard)/crm/prospects/ProspectTable.js
+++ src/app/(dashboard)/crm/prospects/ProspectTable.js
@@ -297,7 +297,7 @@ const ProspectTable = () => {
 
   return (
     <>
-      <div className="reflative">
+      <div className="relative bg-white shadow-md rounded-lg px-4 py-4">
         <Table
           data_to_export={data_to_export()}
           columns_export={columns_export}
