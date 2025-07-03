1d94ad9de76f29a22ce424526d4d4e0e081e6379
airout <a.airout@gmail.com>
2025-07-03 22:06:00 +0100

    fi password requirements

--- src/app/(dashboard)/Utilisateurs/Ajouter-Utilisateur/page.js
+++ src/app/(dashboard)/Utilisateurs/Ajouter-Utilisateur/page.js
@@ -1,11 +1,11 @@
-'use client';
-import React, { useState, useRef, useEffect } from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
-import { APIURL } from '../../../../configs/api';
+import { APIURL } from "../../../../configs/api";
 import { useSociete } from "../../../../context/SocieteContext";
 import { useAuth } from "../../../../context/AuthContext";
 import { Pencil, Eye, EyeOff } from "lucide-react";
@@ -22,7 +22,8 @@ const Page = () => {
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const fileInputRef = useRef(null);
   const [showPassword, setShowPassword] = useState(false);
-  const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] =
+    useState(false);
 
   useEffect(() => {
     if (user?.role === 1) {
@@ -34,7 +35,7 @@ const Page = () => {
     try {
       const token = localStorage.getItem("accessToken");
       const response = await axios.get(APIURL.SOCIETES, {
-        headers: { Authorization: `Bearer ${token}` }
+        headers: { Authorization: `Bearer ${token}` },
       });
       setSocietes(response.data.societes || []);
     } catch (error) {
@@ -43,8 +44,8 @@ const Page = () => {
   };
 
   const validationSchema = Yup.object().shape({
-    name: Yup.string().required('Le nom est requis'),
-    prenom: Yup.string().required('Le prénom est requis'),
+    name: Yup.string().required("Le nom est requis"),
+    prenom: Yup.string().required("Le prénom est requis"),
     email: Yup.string()
       .trim()
       .email("Email invalide")
@@ -52,7 +53,10 @@ const Page = () => {
         /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
         "Format d'email invalide"
       )
-      .notOneOf(["test@test.com", "example@example.com"], "Cet email est interdit")
+      .notOneOf(
+        ["test@test.com", "example@example.com"],
+        "Cet email est interdit"
+      )
       .required("L'email est requis"),
     //role: Yup.string().required('Le rôle est requis'),
     //gender: Yup.string().required('Le genre est requis'),
@@ -63,36 +67,40 @@ const Page = () => {
     fonction: Yup.string(),
     date_embauche: Yup.date(),
     password: Yup.string()
-    .min(8, '• Au moins 8 caractères')
-    .matches(/^(?=.*[A-Z])/, '• Au moins une majuscule')
-    .matches(/^(?=.*[0-9])/, '• Au moins un chiffre')
-    .matches(/^(?=.*[@$!%*?&])/, '• Au moins un caractère spécial')
-    .required('Mot de passe requis'),
+      .min(8, "• Au moins 8 caractères")
+      .required("Mot de passe requis"),
+    // .matches(/^(?=.*[A-Z])/, '• Au moins une majuscule')
+    // .matches(/^(?=.*[0-9])/, '• Au moins un chiffre')
+    // .matches(/^(?=.*[@$!%*?&])/, '• Au moins un caractère spécial')
+    //.required('Mot de passe requis'),
     password_confirmation: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Les mots de passe ne correspondent pas')
-      .required('Confirmation du mot de passe requise'),
+      .oneOf(
+        [Yup.ref("password"), null],
+        "Les mots de passe ne correspondent pas"
+      )
+      .required("Confirmation du mot de passe requise"),
   });
 
   const formik = useFormik({
     initialValues: {
-      name: '',
-      prenom: '',
-      email: '',
-      adresse: '',
+      name: "",
+      prenom: "",
+      email: "",
+      adresse: "",
       photo: null,
-      role: '',
-      password: '',
-      password_confirmation: '',
-      societe_id: selectedSociete?.id || '',
-      gender: '',
-      phone: '',
-      cin: '',
-      fonction: '',
-      date_embauche: '',
-      niveau_etude: '',
-      cnss: '',
-      is_actif: '1',
-      solde_conge: '',
+      role: "",
+      password: "",
+      password_confirmation: "",
+      societe_id: selectedSociete?.id || "",
+      gender: "",
+      phone: "",
+      cin: "",
+      fonction: "",
+      date_embauche: "",
+      niveau_etude: "",
+      cnss: "",
+      is_actif: "1",
+      solde_conge: "",
     },
     validationSchema, // Your Yup validation schema
     onSubmit: async (values) => {
@@ -104,14 +112,14 @@ const Page = () => {
           setLoading(false);
           return;
         }
-  
+
         const formData = new FormData();
         Object.keys(values).forEach((key) => {
           if (values[key] !== null && values[key] !== "") {
             formData.append(key, values[key]);
           }
         });
-  
+
         const response = await axios.post(APIURL.UTILISATEURS, formData, {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -121,15 +129,16 @@ const Page = () => {
         toast.success("Utilisateur ajouté avec succès");
         router.push("/Utilisateurs");
       } catch (error) {
-        toast.error(error.response?.data?.message || "Une erreur est survenue.");
+        toast.error(
+          error.response?.data?.message || "Une erreur est survenue."
+        );
       } finally {
         setLoading(false);
       }
     },
     validateOnChange: false, // Disable validation on input changes
-    validateOnBlur: false,   // Disable validation on blur events
+    validateOnBlur: false, // Disable validation on blur events
   });
-  
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -138,24 +147,39 @@ const Page = () => {
         toast.error("Le fichier est trop volumineux. Taille maximale : 5 Mo");
         return;
       }
-      if (!['image/jpeg', 'image/png'].includes(file.type)) {
+      if (!["image/jpeg", "image/png"].includes(file.type)) {
         toast.error("Type de fichier invalide. Formats acceptés : JPEG ou PNG");
         return;
       }
       setImageFileUrl(URL.createObjectURL(file));
-      formik.setFieldValue('photo', file);
+      formik.setFieldValue("photo", file);
     }
   };
 
   return (
-    <div className='flex justify-center mt-2 bg-white h-[89vh] shadow-md rounded-lg p-4 overflow-auto'>
-      <div className='flex flex-col gap-4 items-center w-full'>
-        <h1 className='text-2xl font-semibold mt-2'>Ajouter un utilisateur</h1>
-        <form onSubmit={formik.handleSubmit} className='flex flex-col items-center w-full'>
-          <input type='file' accept='image/*' onChange={handleImageChange} ref={fileInputRef} className='hidden' />
-          <div className='relative w-32 h-32 mt-6 cursor-pointer' onClick={() => fileInputRef.current?.click()}>
+    <div className="flex justify-center mt-2 bg-white h-[89vh] shadow-md rounded-lg p-4 overflow-auto">
+      <div className="flex flex-col gap-4 items-center w-full">
+        <h1 className="text-2xl font-semibold mt-2">Ajouter un utilisateur</h1>
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex flex-col items-center w-full"
+        >
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            ref={fileInputRef}
+            className="hidden"
+          />
+          <div
+            className="relative w-32 h-32 mt-6 cursor-pointer"
+            onClick={() => fileInputRef.current?.click()}
+          >
             <img
-              src={imageFileUrl || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"}
+              src={
+                imageFileUrl ||
+                "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+              }
               alt="User avatar"
               className="w-full h-full rounded-full object-cover border-8 border-gray-100 shadow-md"
             />
@@ -163,18 +187,47 @@ const Page = () => {
               <Pencil className="text-gray-600 w-5 h-5" />
             </div>
           </div>
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-16 md:gap-y-3 gap-y-4 p-4 '>
-            <Input label='Nom :' type='text' name="name" required value={formik.values.name} onChange={formik.handleChange} {...formik.getFieldProps('name')} error={formik.errors.name} />
-            <Input label='Prénom :' type='text' name="prenom" required value={formik.values.prenom} onChange={formik.handleChange} {...formik.getFieldProps('prenom')} error={formik.errors.prenom}/>
-            <Input label='Email :' type='email' name="email" required value={formik.values.email} onChange={formik.handleChange} {...formik.getFieldProps('email')} error={formik.errors.email}/>
-            <SelectInput 
-              label="Rôle" 
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-16 md:gap-y-3 gap-y-4 p-4 ">
+            <Input
+              label="Nom :"
+              type="text"
+              name="name"
+              required
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              {...formik.getFieldProps("name")}
+              error={formik.errors.name}
+            />
+            <Input
+              label="Prénom :"
+              type="text"
+              name="prenom"
+              required
+              value={formik.values.prenom}
+              onChange={formik.handleChange}
+              {...formik.getFieldProps("prenom")}
+              error={formik.errors.prenom}
+            />
+            <Input
+              label="Email :"
+              type="email"
+              name="email"
+              required
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              {...formik.getFieldProps("email")}
+              error={formik.errors.email}
+            />
+            <SelectInput
+              label="Rôle"
               name="role"
               placeholder="Sélectionnez un rôle"
               options={[
-                ...(user?.role === 1 && !selectedSociete ? [{ label: 'Super Admin', value: '1' }] : []),
-                { label: 'Admin', value: '2' },
-                { label: 'Commercial', value: '3' }
+                ...(user?.role === 1 && !selectedSociete
+                  ? [{ label: "Super Admin", value: "1" }]
+                  : []),
+                { label: "Admin", value: "2" },
+                { label: "Commercial", value: "3" },
               ]}
               value={formik.values.role} // Directly link the value from Formik
               onChange={(value) => formik.setFieldValue("role", value)} // Handle change via setFieldValue
@@ -185,29 +238,35 @@ const Page = () => {
               label="Société"
               name="societe_id"
               placeholder="Sélectionnez une société"
-              options={societes.map(societe => ({ label: societe.raison_sociale, value: societe.id }))}
+              options={societes.map((societe) => ({
+                label: societe.raison_sociale,
+                value: societe.id,
+              }))}
               value={formik.values.societe_id}
               onChange={(value) => formik.setFieldValue("societe_id", value)}
             />
-            <SelectInput 
-              label="Genre" 
+            <SelectInput
+              label="Genre"
               name="gender"
               placeholder="Sélectionnez un genre"
-              options={[{ label: 'Homme', value: '1' }, { label: 'Femme', value: '2' }]}
+              options={[
+                { label: "Homme", value: "1" },
+                { label: "Femme", value: "2" },
+              ]}
               value={formik.values.gender} // Explicitly set the value
               onChange={(value) => formik.setFieldValue("gender", value)} // Handle change with Formik
               error={formik.errors.gender} // Show error if validation fails
             />
             <Input
-              label='Téléphone'
-              type='text'
+              label="Téléphone"
+              type="text"
               name="phone"
               value={formik.values.phone}
               onChange={(e) => {
                 // Filter to allow only numbers
-                const numericValue = e.target.value.replace(/[^0-9]/g, '');
+                const numericValue = e.target.value.replace(/[^0-9]/g, "");
                 // Update Formik's value
-                formik.setFieldValue('phone', numericValue);
+                formik.setFieldValue("phone", numericValue);
               }}
               onKeyPress={(e) => {
                 // Prevent non-numeric key presses
@@ -217,9 +276,17 @@ const Page = () => {
               }}
               error={formik.errors.phone}
               inputMode="numeric" // Shows numeric keyboard on mobile
-              pattern="[0-9]*"   // Additional hint for numeric input
+              pattern="[0-9]*" // Additional hint for numeric input
             />
-            <Input label="CIN" type="text" name="cin" value={formik.values.cin} onChange={formik.handleChange} {...formik.getFieldProps('cin')} error={formik.errors.cin}/>
+            <Input
+              label="CIN"
+              type="text"
+              name="cin"
+              value={formik.values.cin}
+              onChange={formik.handleChange}
+              {...formik.getFieldProps("cin")}
+              error={formik.errors.cin}
+            />
             <DateInput
               label="Date d'embauche"
               name="date_embauche"
@@ -232,40 +299,67 @@ const Page = () => {
               name="niveau_etude"
               placeholder="Sélectionnez un niveau d'étude"
               options={[
-                { label: 'Bac', value: '1' },
-                { label: 'Bac+2', value: '2' },
-                { label: 'Bac+3', value: '3' },
-                { label: 'Bac+5', value: '4' },
-                { label: 'Bac+8', value: '5' }
+                { label: "Bac", value: "1" },
+                { label: "Bac+2", value: "2" },
+                { label: "Bac+3", value: "3" },
+                { label: "Bac+5", value: "4" },
+                { label: "Bac+8", value: "5" },
               ]}
               value={formik.values.niveau_etude}
               onChange={(value) => formik.setFieldValue("niveau_etude", value)}
             />
-            <Input label='Adresse' type='text' name="adresse" value={formik.values.adresse} onChange={formik.handleChange} />
-            <Input label='CNSS' type='text' name="cnss" value={formik.values.cnss} onChange={formik.handleChange} />
+            <Input
+              label="Adresse"
+              type="text"
+              name="adresse"
+              value={formik.values.adresse}
+              onChange={formik.handleChange}
+            />
+            <Input
+              label="CNSS"
+              type="text"
+              name="cnss"
+              value={formik.values.cnss}
+              onChange={formik.handleChange}
+            />
             <SelectInput
               label="Actif"
               name="is_actif"
               placeholder="Sélectionnez un statut"
               options={[
-                { label: 'Actif', value: '1' },
-                { label: 'Inactif', value: '0' }
+                { label: "Actif", value: "1" },
+                { label: "Inactif", value: "0" },
               ]}
               value={formik.values.is_actif} // Use formik.values.is_actif
               onChange={(value) => formik.setFieldValue("is_actif", value)} // Use formik.setFieldValue
               error={formik.errors.is_actif} // Add error for validation
             />
-            <Input label="Fonction" type="text" name="fonction" 
-            value={formik.values.fonction}
-            onChange={formik.handleChange}
-            error={formik.errors.fonction} />
-            <Input label="Solde de congé" type="number" name="solde_conge" value={formik.values.solde_conge} onChange={formik.handleChange} />
+            <Input
+              label="Fonction"
+              type="text"
+              name="fonction"
+              value={formik.values.fonction}
+              onChange={formik.handleChange}
+              error={formik.errors.fonction}
+            />
+            <Input
+              label="Solde de congé"
+              type="number"
+              name="solde_conge"
+              value={formik.values.solde_conge}
+              onChange={formik.handleChange}
+            />
             {/* password input with toggle view hide */}
-            <Input label="Mot de passe" type={showPassword ? "text" : "password"} name="password" required
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            error={formik.errors.password}>
-            {showPassword ? (
+            <Input
+              label="Mot de passe"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              required
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.errors.password}
+            >
+              {showPassword ? (
                 <EyeOff
                   className="w-6 h-6 !text-gray-600"
                   onClick={() => setShowPassword(false)}
@@ -278,11 +372,16 @@ const Page = () => {
               )}
             </Input>
             {/* confirm password input with toggle view hide */}
-            <Input label="Confirmer le mot de passe" type={showPasswordConfirmation ? "text" : "password"} name="password_confirmation" required
-            value={formik.values.password_confirmation}
-            onChange={formik.handleChange}
-            error={formik.errors.password_confirmation}>
-            {showPasswordConfirmation ? (
+            <Input
+              label="Confirmer le mot de passe"
+              type={showPasswordConfirmation ? "text" : "password"}
+              name="password_confirmation"
+              required
+              value={formik.values.password_confirmation}
+              onChange={formik.handleChange}
+              error={formik.errors.password_confirmation}
+            >
+              {showPasswordConfirmation ? (
                 <EyeOff
                   className="w-6 h-6 !text-gray-600"
                   onClick={() => setShowPasswordConfirmation(false)}
@@ -294,19 +393,29 @@ const Page = () => {
                 />
               )}
             </Input>
-            
           </div>
           <div className="flex gap-4 items-center mt-6 mb-6">
-            <button type="button" className="bg-gray-400 text-white font-medium rounded-lg px-6 py-2" onClick={() => router.push('/Utilisateurs')}>Annuler</button>
-            <button type="submit" className={`bg-[#2D8548] text-white font-medium rounded-lg px-6 py-2 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={loading}>
-              {loading ? 'Ajout en cours...' : 'Ajouter'}
+            <button
+              type="button"
+              className="bg-gray-400 text-white font-medium rounded-lg px-6 py-2"
+              onClick={() => router.push("/Utilisateurs")}
+            >
+              Annuler
+            </button>
+            <button
+              type="submit"
+              className={`bg-[#2D8548] text-white font-medium rounded-lg px-6 py-2 ${
+                loading ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+              disabled={loading}
+            >
+              {loading ? "Ajout en cours..." : "Ajouter"}
             </button>
           </div>
         </form>
       </div>
     </div>
-);
-
+  );
 };
 
 export default Page;
