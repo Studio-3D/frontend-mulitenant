ddfc5fa886f0310b14c35dbba92797d4ea2a8704
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2025-07-09 16:20:00 +0100

    adding marging and fixing hover translate

--- src/components/statistique/StatCard.js
+++ src/components/statistique/StatCard.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export const StatCard = ({ title, value, change, isPositive, icon, color }) => {
   return (
-    <div className="bg-white rounded-lg shadow transition-transform hover:shadow-md hover:-translate-y-1 duration-300">
+    <div className="bg-white rounded-lg shadow mt-1 transition-transform hover:shadow-md hover:-translate-y-1 duration-300">
       <div className="p-5">
         <div className="flex items-center justify-between">
           <div>
             <p className="text-sm font-medium text-gray-500">{title}</p>
-            <p className="text-2xl font-bold mt-1">{value}</p>
+            <p className="text-xl font-bold mt-1">{value}</p>
           </div>
           <div className={`p-3 rounded-full ${color} text-white`}>{icon}</div>
         </div>
