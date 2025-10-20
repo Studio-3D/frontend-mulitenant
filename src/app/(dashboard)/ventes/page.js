dcd8a1a1bc090537a440ac53bbee8e64e7a1db51
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2025-10-20 14:49:14 +0100

    crm navbar and replace by new tabs

--- src/app/(dashboard)/ventes/page.js
+++ src/app/(dashboard)/ventes/page.js
@@ -3,9 +3,6 @@ import { useRouter } from 'next/navigation';
 import { VentePage } from '../../../components/Vente/ventePage'
 
 export default function page() {
-  const router = useRouter();
-  
-  
   return (
     <div className="w-full min-h-[89vh] bg-white rounded-lg shadow-md p-4">
       <VentePage />
